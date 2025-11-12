3a4139a7a7bf461855654e939f8ee3457a1e0fdb
Byron Ochara <byron.ochara@gmail.com>
2025-11-12 18:42:03 +0300

    imported mode toggle and rmoved bg color

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,10 +3,11 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
+import ModeToggle from "./mode-toggle";
 
 const Header = () => {
   return (
-    <header className="w-full border-b bg-stone-600">
+    <header className="w-full border-b">
       <div className="wrapper flex-between">
         <div className="flex-start">
           <Link href="/" className="flex-start">
@@ -23,6 +24,7 @@ const Header = () => {
           </Link>
         </div>
         <div className="space-x-2">
+          <ModeToggle />
           <Button asChild variant="ghost">
             <Link href="/cart">
               <ShoppingCart /> Cart
