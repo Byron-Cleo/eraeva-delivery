035c3b68df3cf25fa7b91a5984758ec53afb9852
Byron Ochara <byron.ochara@gmail.com>
2025-11-13 08:36:03 +0300

    updated the header with menu component

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,15 +1,13 @@
-import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
-import ModeToggle from "./mode-toggle";
+import Menu from "./menu";
 
 const Header = () => {
   return (
-    <header className="w-full border-b">
-      <div className="wrapper flex-between">
-        <div className="flex-start">
+    <header className="w-full border-b bg-black">
+      <div className="wrapper flex-between bg-gray-500">
+        <div className="flex-start bg-blue-400">
           <Link href="/" className="flex-start">
             <Image
               src="/images/logo.svg"
@@ -23,19 +21,7 @@ const Header = () => {
             </span>
           </Link>
         </div>
-        <div className="space-x-2">
-          <ModeToggle />
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingCart /> Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon /> Sign In
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   );
