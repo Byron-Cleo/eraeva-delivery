efc77ce605cbb4947e033f00f84f9acfe6d0f161
Byron Ochara <byron.ochara@gmail.com>
2025-11-13 16:32:33 +0300

    custed number from .env file

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -3,4 +3,5 @@ export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION || "A modern Next.js App ";
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
-export const LATEST_PRODUCTS_LIMIT = 4;
+export const LATEST_PRODUCTS_LIMIT =
+  Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
