abd9e9df41835dc909fc057cd1f640ba0da4a85c
Byron Ochara <byron.ochara@gmail.com>
2025-11-14 20:32:43 +0300

    removed the seed file for deployment

--- db/seed.ts
+++ db/seed.ts
@@ -1,12 +1,12 @@
+// import { PrismaClient } from '../lib/generated/prisma'
 // import { PrismaClient } from '@prisma/client';
-import { prisma } from "@/db/prisma";
-import sampleData from "./sample-data";
+// import sampleData from "./sample-data";
 
-async function main() {
-  // const prisma = new PrismaClient();
-  await prisma.product.deleteMany();
-  await prisma.product.createMany({ data: sampleData.products });
-  console.log("Database seeded successfullly");
-}
+// async function main() {
+//   const prisma = new PrismaClient();
+//   await prisma.product.deleteMany();
+//   await prisma.product.createMany({ data: sampleData.products });
+//   console.log("Database seeded successfullly");
+// }
 
-main();
+// main();
