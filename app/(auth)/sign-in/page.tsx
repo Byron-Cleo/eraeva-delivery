09d1625db0019dd692d235b5b070a7631dbb8364
Byron Ochara <byron.ochara@gmail.com>
2025-11-29 20:32:49 +0300

    rearranged code and addd redirect onLogin

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import { auth } from "@/auth";
 import {
   Card,
   CardContent,
@@ -6,16 +11,19 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { APP_NAME } from "@/lib/constants";
-import { Metadata } from "next";
-import Image from "next/image";
-import Link from "next/link";
 import CredentialsSignInForm from "./credentials-signin-form";
 
 export const metadata: Metadata = {
   title: "Sign In",
 };
 
-const SignInPage = () => {
+const SignInPage = async () => {
+  const session = await auth();
+
+  if (session) {
+    return redirect("/");
+  }
+
   return (
     <div className="w-full max-w-md mx-auto">
       <Card>
