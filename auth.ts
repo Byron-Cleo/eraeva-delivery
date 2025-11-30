de63e979e92e6f47aa8c0dae1207be70ec7afff3
Byron Ochara <byron.ochara@gmail.com>
2025-11-30 16:24:45 +0300

    adding role to session and jwt

--- auth.ts
+++ auth.ts
@@ -3,7 +3,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/db/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
-import type {NextAuthConfig} from "next-auth"
+import type { NextAuthConfig } from "next-auth";
 
 export const config = {
   pages: {
@@ -22,46 +22,69 @@ export const config = {
         password: { type: "password" },
       },
       async authorize(credentials) {
-        if(credentials === null) return null;
+        if (credentials === null) return null;
 
         //find user in the databse
-        const user = await  prisma.user.findFirst({ where: {email: credentials.email as string}});
+        const user = await prisma.user.findFirst({
+          where: { email: credentials.email as string },
+        });
 
         //check if user exists and if the pass matches
-        if(user  && user.password) {
-           const isMatch = compareSync(credentials.password as string , user.password)
-           
-           //if the password is correct, return the user
-           if(isMatch) {
+        if (user && user.password) {
+          const isMatch = compareSync(
+            credentials.password as string,
+            user.password
+          );
+
+          //if the password is correct, return the user
+          if (isMatch) {
             return {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                role: user.role,
-            }
-           }
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            };
+          }
         }
 
         //if user doesn't exist or password does not match, return null
-        return null
-      }
+        return null;
+      },
     }),
   ],
   callbacks: {
     async session({ session, user, trigger, token }: any) {
-        //set the user ID from the token
-        session.user.id = token.sub
+      //set the user ID from the token
+      session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
 
-        //if there is an update, set the user name
-        if(trigger === "update") {
-            session.user.name = user.name
-        }
+      //if there is an update, set the user name
+      if (trigger === "update") {
+        session.user.name = user.name;
+      }
 
-      return session
+      return session;
     },
-}
+    async jwt({ token, user, trigger, session }: any) {
+      // assign user info to the token on sign in
+      if (user) {
+        token.role = user.role;
 
+        //if user has no name the use the email as name
+        if (user.name === null || user.name === "NO_NAME") {
+          token.name = user.email.split("@")[0];
 
+          //update database to reflect the token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+      return token;
+    },
+  },
 } satisfies NextAuthConfig;
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
