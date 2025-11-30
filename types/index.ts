981309148cf7be245e3846395963f5f5a633b1a7
Byron Ochara <byron.ochara@gmail.com>
2025-11-30 17:46:26 +0300

    creating cart schemas for creating data types for typescript

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,13 @@
 import {z} from 'zod';
-import { insertProductSchema } from '@/lib/validators';
+import { insertProductSchema, cartItemSchema, insertCartSchema } from '@/lib/validators';
 
 export type Product = z.infer<typeof insertProductSchema> & {
     id: string,
     rating: string,
     createdAt: Date,
 }
+
+//here we are inferring the carts schemas type to create a TypeScript type
+export type Cart = z.infer<typeof insertCartSchema>
+
+export type CartItem = z.infer<typeof cartItemSchema>
