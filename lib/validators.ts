e1261fec0ed7021aacbc48812d62e9d79a0a61f5
Byron Ochara <byron.ochara@gmail.com>
2026-02-15 18:10:33 +0300

    chnaged min characters to 5

--- lib/validators.ts
+++ lib/validators.ts
@@ -25,7 +25,7 @@ export const insertProductSchema = z.object({
 //Schema for signing users in
 export const signInFormSchema = z.object({
   email: z.string().email("Invalid email address."),
-  password: z.string().min(6, "Password must be at least 6 characters.")
+  password: z.string().min(5, "Password must be at least 5 characters.")
 })
 
 //Schema for signing up a users
