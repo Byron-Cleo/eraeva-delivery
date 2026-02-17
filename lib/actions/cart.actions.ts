a7e5251e550c5ca943a7e842d29e175b25e7dcd7
Byron Ochara <byron.ochara@gmail.com>
2026-02-18 01:52:58 +0300

    code modification

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -1,7 +1,12 @@
 "use server";
 
 import { cookies } from "next/headers";
-import { convertToPlainObject, formatError, round2, formatNumberWithDecimal } from "@/lib/utils";
+import {
+  convertToPlainObject,
+  formatError,
+  round2,
+  formatNumberWithDecimal,
+} from "@/lib/utils";
 import { CartItem } from "@/types";
 import { auth } from "@/auth";
 import { prisma } from "@/db/prisma";
@@ -25,43 +30,41 @@ const calcPrice = (items: CartItem[]) => {
   };
 };
 
-
-
 export async function addItemToCart(data: { item: CartItem }) {
-    try {
-        // check for cart cookie
-        const sessionCartId = (await cookies()).get("sessionCartId")?.value;
-        if (!sessionCartId) throw new Error("Cart session not found");
-        
-        //Get session and user ID
-        //a. getting session
-        const session = await auth();
-        //b. getting user id
-        const userId = session?.user?.id ? (session.user.id as string) : undefined;
-        
-        //get user cart from the database
-        const cart = await getMyCart();
-        
-        //parse and validate added item
-        const item = cartItemSchema.parse(data.item);
-        
-        //find the product from the database
-        const product = await prisma.product.findFirst({
-            where: { id: item.productId },
-        });
-        if (!product) throw new Error("Product not found");
-        
-        //creating new cart object
-        if (!cart) {
-            const newCart = insertCartSchema.parse({
-                userId: userId,
-                items: [item],
-                sessionCartId: sessionCartId,
-        ...calcPrice([item]),
+  try {
+    // check for cart cookie
+    const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+    if (!sessionCartId) throw new Error("Cart session not found");
+
+    //Get session and user ID
+    //a. getting session
+    const session = await auth();
+    //b. getting user id
+    const userId = session?.user?.id ? (session.user.id as string) : undefined;
+
+    //get user cart from the database
+    const cart = await getMyCart();
+
+    //parse and validate added item
+    const item = cartItemSchema.parse(data.item);
+
+    //find the product from the database
+    const product = await prisma.product.findFirst({
+      where: { id: item.productId },
     });
-      
+    if (!product) throw new Error("Product not found");
+
+    //creating new cart object
+    if (!cart) {
+      const newCart = insertCartSchema.parse({
+        userId: userId,
+        items: [item],
+        sessionCartId: sessionCartId,
+        ...calcPrice([item]),
+      });
+
       // Add the new cart to database
-      await prisma.cart.create({data: newCart});
+      await prisma.cart.create({ data: newCart });
 
       //Revalidate the product page: purpose to clear the cache for a particular path
       revalidatePath(`/product/${product.slug}`);
@@ -70,8 +73,10 @@ export async function addItemToCart(data: { item: CartItem }) {
         success: true,
         message: "Item added to cart successfully",
       };
+    } else {
 
     }
+    
   } catch (error) {
     return {
       success: false,
