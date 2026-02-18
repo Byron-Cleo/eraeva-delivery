611a929c59852a2fdc74f4422ac59f32a8c29a81
Byron Ochara <byron.ochara@gmail.com>
2026-02-18 18:37:57 +0300

    get cart and ui update

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -5,6 +5,7 @@ import ProductPrice from "@/components/shared/products/product-price";
 import { getProductBySlug } from "@/lib/actions/product.actions";
 import ProductImages from "@/components/shared/products/product-images";
 import AddToCart from "@/components/shared/products/add-to-cart";
+import { getMyCart } from "@/lib/actions/cart.actions";
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -13,6 +14,7 @@ const ProductDetailsPage = async (props: {
   const product = await getProductBySlug(slug);
   if (!product) notFound();
 
+  const cart = await getMyCart();
   return (
     <>
       <section>
@@ -29,10 +31,10 @@ const ProductDetailsPage = async (props: {
               <p>
                 {product.rating} of {product.numReviews} Reviews
               </p>
-              <div className="flex flex-col sm:flex-row sm:items-center gap-3">
+              <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
                 <ProductPrice
                   value={Number(product.price)}
-                  className="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2"
+                  className="w-24 px-5 py-2 text-green-700 bg-green-100 rounded-full"
                 />
               </div>
             </div>
@@ -45,13 +47,13 @@ const ProductDetailsPage = async (props: {
           <div>
             <Card>
               <CardContent className="p-4">
-                <div className="mb-2 flex justify-between">
+                <div className="flex justify-between mb-2">
                   <div>Price</div>
                   <div>
                     <ProductPrice value={Number(product.price)} />
                   </div>
                 </div>
-                <div className="mb-2 flex justify-between">
+                <div className="flex justify-between mb-2">
                   <div>Status</div>
                   {product.stock > 0 ? (
                     <Badge variant="outline">In Stock</Badge>
@@ -61,7 +63,9 @@ const ProductDetailsPage = async (props: {
                 </div>
                 {product.stock > 0 && (
                   <div className="flex-center">
-                    <AddToCart item={{
+                    <AddToCart 
+                    cart={cart} 
+                    item ={{
                       productId: product.id,
                       name: product.name,
                       slug: product.slug,
