5c88aee5b9783d1d8e54e6e4e94d7a777f7d0d29
Byron Ochara <byron.ochara@gmail.com>
2026-02-19 22:37:39 +0300

    formattng and added checkoutsteps component

--- app/(root)/shipping-address/page.tsx
+++ app/(root)/shipping-address/page.tsx
@@ -1,30 +1,36 @@
 import { auth } from "@/auth";
 import { getMyCart } from "@/lib/actions/cart.actions";
 import { Metadata } from "next";
-import { redirect} from "next/navigation";
+import { redirect } from "next/navigation";
 import { ShippingAddress } from "@/types";
-import {getUserById} from "@/lib/actions/user.actions";
+import { getUserById } from "@/lib/actions/user.actions";
 import ShippingAddressForm from "./shipping-address-form";
+import CheckoutSteps from "@/components/shared/checkout-steps/checkout-steps";
 
 export const metadata: Metadata = {
   title: "Shipping Address",
 };
 
 const ShippingAddressPage = async () => {
-    const cart = await getMyCart();
-    
-    if(!cart || cart.items.length === 0) {
-        redirect("/cart");
-    }
-    
-    //get userId from session 
-    const session = await auth()
-    const userId = session?.user?.id;
-    if(!userId) throw new Error("User has not id");
-    
-    const user = await getUserById(userId);
+  const cart = await getMyCart();
 
-  return <div><ShippingAddressForm address={user.address as ShippingAddress} /> </div>;
+  if (!cart || cart.items.length === 0) {
+    redirect("/cart");
+  }
+
+  //get userId from session
+  const session = await auth();
+  const userId = session?.user?.id;
+  if (!userId) throw new Error("User has not id");
+
+  const user = await getUserById(userId);
+
+  return (
+    <>
+      <CheckoutSteps current={1} />
+      <ShippingAddressForm address={user.address as ShippingAddress} />
+    </>
+  );
 };
 
 export default ShippingAddressPage;
