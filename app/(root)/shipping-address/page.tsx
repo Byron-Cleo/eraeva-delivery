c905a8bcafd26db210d546993563148174a5bfb0
Byron Ochara <byron.ochara@gmail.com>
2026-02-19 17:51:42 +0300

    created shipping address form and display page

--- app/(root)/shipping-address/page.tsx
+++ app/(root)/shipping-address/page.tsx
@@ -1,5 +1,31 @@
-const ShippingAddressPage = () => {
-  return <div>Shipping Address Page</div>;
+import { auth } from "@/auth";
+import { getMyCart } from "@/lib/actions/cart.actions";
+import { Metadata } from "next";
+import { redirect} from "next/navigation";
+import { ShippingAddress } from "@/types";
+import {getUserById} from "@/lib/actions/user.actions";
+import ShippingAddressForm from "./shipping-address-form";
+
+export const metadata: Metadata = {
+  title: "Shipping Address",
+};
+
+const ShippingAddressPage = async () => {
+    const cart = await getMyCart();
+    
+    if(!cart || cart.items.length === 0) {
+        redirect("/cart");
+    }
+    
+    //get userId from session 
+    const session = await auth()
+    const userId = session?.user?.id;
+    if(!userId) throw new Error("User has not id");
+    
+    const user = await getUserById(userId);
+    console.log("UUU===>>>", user)
+
+  return <div><ShippingAddressForm address={user.address as ShippingAddress} /> </div>;
 };
 
 export default ShippingAddressPage;
