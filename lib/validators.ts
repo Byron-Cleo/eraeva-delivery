ae77db4a7f08eee8aea9fdb8805bb1e5218b2a97
Byron Ochara <byron.ochara@gmail.com>
2026-02-20 22:39:57 +0300

    validator and schema for payment method

--- lib/validators.ts
+++ lib/validators.ts
@@ -1,11 +1,12 @@
 import { z } from "zod";
 import { formatNumberWithDecimal } from "./utils";
+import { PAYMENT_METHODS } from "@/lib/constants";
 
 const currency = z
   .string()
   .refine(
     (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))),
-    "Price must have exactly two decimal places"
+    "Price must have exactly two decimal places",
   );
 
 //schema for inserting products
@@ -25,46 +26,58 @@ export const insertProductSchema = z.object({
 //Schema for signing users in
 export const signInFormSchema = z.object({
   email: z.string().email("Invalid email address."),
-  password: z.string().min(5, "Password must be at least 5 characters.")
-})
+  password: z.string().min(5, "Password must be at least 5 characters."),
+});
 
 //Schema for signing up a users
-export const signUpFormSchema = z.object({
-  name: z.string().min(3, "Name must be at least 3 characters."),
-  email: z.string().email("Invalid email address."),
-  password: z.string().min(5, "Password must be at least 5 characters."),
-  confirmPassword: z.string().min(5, "Confirm password must be at least 5 characters.")
-}).refine((data) => data.password === data.confirmPassword, {
-  message: "Passwords do not match.",
-  path: ["confirmPassword"],});
-
-//cart schemas
-  export const cartItemSchema = z.object({
-    productId: z.string().min(1, "Product is required."),
-    name: z.string().min(1, "Name is required."),
-    slug: z.string().min(1, "Slug is required."),
-    qty: z.number().int().nonnegative("Quantity must be a positive number."),
-    image: z.string().min(1, "Image is required."),
-    price: currency,
+export const signUpFormSchema = z
+  .object({
+    name: z.string().min(3, "Name must be at least 3 characters."),
+    email: z.string().email("Invalid email address."),
+    password: z.string().min(5, "Password must be at least 5 characters."),
+    confirmPassword: z
+      .string()
+      .min(5, "Confirm password must be at least 5 characters."),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match.",
+    path: ["confirmPassword"],
   });
 
-  export const insertCartSchema = z.object({
-    items: z.array(cartItemSchema),
-    itemsPrice: currency,
-    shippingPrice: currency,
-    taxPrice: currency,
-    totalPrice: currency,
-    sessionCartId: z.string().min(1, "Session Cart ID is required."),
-    userId: z.string().optional().nullable(),
-  })
+//cart schemas
+export const cartItemSchema = z.object({
+  productId: z.string().min(1, "Product is required."),
+  name: z.string().min(1, "Name is required."),
+  slug: z.string().min(1, "Slug is required."),
+  qty: z.number().int().nonnegative("Quantity must be a positive number."),
+  image: z.string().min(1, "Image is required."),
+  price: currency,
+});
 
+export const insertCartSchema = z.object({
+  items: z.array(cartItemSchema),
+  itemsPrice: currency,
+  shippingPrice: currency,
+  taxPrice: currency,
+  totalPrice: currency,
+  sessionCartId: z.string().min(1, "Session Cart ID is required."),
+  userId: z.string().optional().nullable(),
+});
 
-  export const shippingAddressSchema = z.object({
-    fullName: z.string().min(3, "Full name must be at least 3 characters."),
-    streetAddress: z.string().min(3, "Address must be at least 3 characters."),
-    city: z.string().min(2, "City must be at least 2 characters."),
-    postalCode: z.string().min(3, "Postal code must be at least 3 characters."),
-    country: z.string().min(3, "Country must be at least 3 characters."),
-    lat: z.number().optional(),
-    lng: z.number().optional()
-  })
+export const shippingAddressSchema = z.object({
+  fullName: z.string().min(3, "Full name must be at least 3 characters."),
+  streetAddress: z.string().min(3, "Address must be at least 3 characters."),
+  city: z.string().min(2, "City must be at least 2 characters."),
+  postalCode: z.string().min(3, "Postal code must be at least 3 characters."),
+  country: z.string().min(3, "Country must be at least 3 characters."),
+  lat: z.number().optional(),
+  lng: z.number().optional(),
+});
+
+//schema for payment method
+export const paymentMethodSchema = z
+  .object({ type: z.string().min(1, "Payment method is required.") })
+  .refine((data) => PAYMENT_METHODS.includes(data.type), {
+    path: ["type"],
+    message: "Invalid payment method",
+  });
