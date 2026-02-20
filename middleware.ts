d70e99c7ec5fdf59b6d2b1261d29d44ce5ec30c3
Byron Ochara <byron.ochara@gmail.com>
2026-02-20 18:13:18 +0300

    updated middleware to check authenticated user on protected paths

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,25 @@
-// import  { auth } from "@/auth";
+import { NextRequest, NextResponse } from "next/server";
 
-// export const middleware = auth;
-
-import { NextRequest, NextResponse } from 'next/server';
-// import {cookies } from "next/headers";
+const protectedPaths = [
+  /\/shipping-address/,
+  /\/payment-method/,
+  /\/place-order/,
+  /\/profile/,
+  /\/user\/(.*)/,
+  /\/order\/(.*)/,
+  /\/admin/,
+];
 
 export function middleware(request: NextRequest) {
+  //check the user is logged in or not logged in by using request's cookies
+  //it identifies the logged in and not yet logged in user
+  const sessionToken = request.cookies.get("authjs.session-token")?.value;
+  const { pathname } = request.nextUrl;
+  if (!sessionToken && protectedPaths.some((p) => p.test(pathname))) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+
+  }
+
   //check for session cart cookie
   if (!request.cookies.get("sessionCartId")) {
     // Generate  new session cart id cookie
@@ -25,8 +39,14 @@ export function middleware(request: NextRequest) {
     response.cookies.set("sessionCartId", sessionCartId);
 
     return response;
-  } else {
-    // return true;
-    return NextResponse.next();
   }
+
+  //Always go to the next response as normal working of request response cycle
+  return NextResponse.next();
 }
+
+// 2. The Matcher Config
+export const config = {
+  matcher: protectedPaths, // Specific paths
+  // matcher: ['/about/:path*', '/dashboard/:path*'], // Specific paths
+};
