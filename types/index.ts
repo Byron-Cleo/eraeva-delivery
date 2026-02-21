11d4d9893123d6b6cde59f999af725d8fa01f8d1
Byron Ochara <byron.ochara@gmail.com>
2026-02-21 12:48:46 +0300

    order types for ui structure

--- types/index.ts
+++ types/index.ts
@@ -1,15 +1,37 @@
-import {z} from 'zod';
-import { insertProductSchema, cartItemSchema, insertCartSchema, shippingAddressSchema } from '@/lib/validators';
+import { z } from "zod";
+import {
+  insertProductSchema,
+  cartItemSchema,
+  insertCartSchema,
+  shippingAddressSchema,
+  insertOrderSchema,
+  insertOrderItemSchema,
+} from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
-    id: string,
-    rating: string,
-    createdAt: Date,
-}
+  id: string;
+  rating: string;
+  createdAt: Date;
+};
 
 //here we are inferring the carts zod schema type to create a TypeScript type
-export type Cart = z.infer<typeof insertCartSchema>
+export type Cart = z.infer<typeof insertCartSchema>;
 
-export type CartItem = z.infer<typeof cartItemSchema>
+export type CartItem = z.infer<typeof cartItemSchema>;
 
-export type ShippingAddress = z.infer<typeof shippingAddressSchema>
+export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+
+//the additional properties are to be added on the database table itself and not
+// from the form as defined in the inserOrderSchema
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string;
+  createdAt: Date;
+  isPaid: Boolean;
+  paidAt: Date | null;
+  isDelivered: Boolean;
+  deliveredAt: Date | null;
+  orderitems: OrderItem[];
+  user: { name: string; email: string };
+};
