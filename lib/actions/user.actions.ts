438b1263622747ef2ae1dc5bfdd8a6d48bb7de6c
Byron Ochara <byron.ochara@gmail.com>
2026-02-21 09:15:38 +0300

    removed arror function for success deployment

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -125,22 +125,4 @@ export async function updateUserAddress(data: ShippingAddress) {
   }
 }
 
-//update user's payment method
-export async function updateUserPaymentMethod(
-  data: z.infer<typeof paymentMethodSchema>,
-) {
-  try {
-    const session = await auth()
-    const currentUser = prisma.user.findFirst({where: {id: session?.user?.id} })
 
-    if(!currentUser) throw new Error("User not found")
-
-    const paymentMethod = paymentMethodSchema.parse(data)
-
-    await prisma.user.update({where: {id: currentUser.id}, data: {paymentMethod: paymentMethod.type}})
-
-    return { success: true, message: "User updated successfully" };
-  } catch (error) {
-    return { success: false, message: formatError(error) };
-  }
-}
