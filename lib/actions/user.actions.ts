da81c42d239248bdb1ae21d047aa11b04a874a22
Byron Ochara <byron.ochara@gmail.com>
2026-02-21 09:10:07 +0300

    updated user actions with updateUserPaymentMethod

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,12 +1,18 @@
 "use server";
 
-import { shippingAddressSchema, signInFormSchema, signUpFormSchema } from "../validators";
+import {
+  paymentMethodSchema,
+  shippingAddressSchema,
+  signInFormSchema,
+  signUpFormSchema,
+} from "../validators";
 import { auth, signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect";
 import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
 import { formatError } from "@/lib/utils";
 import { ShippingAddress } from "@/types";
+import { z } from "zod";
 
 //sign in the user with credentials
 export async function signInWithCredentials(
@@ -86,7 +92,7 @@ export async function getUserById(userId: string) {
     },
   });
 
-  if(!user) throw new Error("User not found.");
+  if (!user) throw new Error("User not found.");
 
   return user;
 }
@@ -113,9 +119,28 @@ export async function updateUserAddress(data: ShippingAddress) {
       },
     });
 
-    return {  success: true, message: "User address updated successfully." };
+    return { success: true, message: "User address updated successfully." };
   } catch (error) {
     return { success: false, message: formatError(error) };
   }
 }
 
+//update user's payment method
+export async function updateUserPaymentMethod(
+  data: z.infer<typeof paymentMethodSchema>,
+) {
+  try {
+    const session = await auth()
+    const currentUser = prisma.user.findFirst({where: {id: session?.user?.id} })
+
+    if(!currentUser) throw new Error("User not found")
+
+    const paymentMethod = paymentMethodSchema.parse(data)
+
+    await prisma.user.update({where: {id: currentUser.id}, data: {paymentMethod: paymentMethod.type}})
+
+    return { success: true, message: "User updated successfully" };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
