bf34939c459e98650dc646b019b0f64ad8e14470
Byron Ochara <byron.ochara@gmail.com>
2026-02-24 21:24:35 +0300

    commented out shipping-address

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 const protectedPaths = [
-  /\/shipping-address/,
+  // /\/shipping-address/,
   /\/payment-method/,
   /\/place-order/,
   /\/profile/,
