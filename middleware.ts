3d75f80a124d946f690e00229a5d3b68b9777a3c
Byron Ochara <byron.ochara@gmail.com>
2026-02-25 00:05:16 +0300

    removed code bit

--- middleware.ts
+++ middleware.ts
@@ -21,8 +21,6 @@ export function middleware(request: NextRequest) {
   // Optional: Redirect authenticated users away from the login page and redirect to their destinatin
   if (isAuthenticated && protectedPaths.some((p) => p.test(pathname))) {
     return NextResponse.redirect(new URL(pathname, request.url));
-  } else {
-      return NextResponse.redirect(new URL("/sign-in", request.url));
   }
   // if (!isAuthenticated && protectedPaths.some((p) => p.test(pathname))) {
   //   return NextResponse.redirect(new URL("/sign-in", request.url));
