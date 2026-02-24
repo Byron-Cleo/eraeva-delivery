32de6fbebac9143b0a8e13408b6457e9c1a01248
Byron Ochara <byron.ochara@gmail.com>
2026-02-24 22:11:31 +0300

    testing changes

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
+import { is } from "zod/v4/locales";
 
 
 export function middleware(request: NextRequest) {
   const protectedPaths = [
-  /\/shipping-address/,
-  /\/payment-method/,
-  /\/place-order/,
-  /\/profile/,
-  /\/user\/(.*)/,
-  /\/order\/(.*)/,
-  /\/admin/,
+  "/shipping-address",
+  "/payment-method",
+  "/place-order",
+  "/profile",
+  "/user/*",
+  "/order/*",
+  "/admin",
 ];
+//   const protectedPaths = [
+//   /\/shipping-address/,
+//   /\/payment-method/,
+//   /\/place-order/,
+//   /\/profile/,
+//   /\/user\/(.*)/,
+//   /\/order\/(.*)/,
+//   /\/admin/,
+// ];
   //check the user is logged in or not logged in by using request's cookies
   //it identifies the logged in and not yet logged in user
   const sessionToken = request.cookies.get("authjs.session-token")?.value;
   const isAuthenticated = !!sessionToken;
-  const { pathname } = request.nextUrl;
-  if (!isAuthenticated && protectedPaths.some((p) => p.test(pathname))) {
+  // console.log("+++++++>>>>>", isAuthenticated);
+  // const { pathname } = request.nextUrl;
+  if (!isAuthenticated && protectedPaths.some((p) => p.startsWith(p))) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 
