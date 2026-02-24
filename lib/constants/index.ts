0ff7815ffbb7edb2d749c33fff9f28cb794d7cfd
Byron Ochara <byron.ochara@gmail.com>
2026-02-24 18:59:41 +0300

    removed user default values

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -7,8 +7,8 @@ export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 
 export const signInDefaultValues = {
-  email: "admin@example.com",
-  password: "12345",
+  email: "",
+  password: "",
 };
 
 export const signUpDefaultValues = {
