07b27532b09aa5ecbd3fb4daf1eeea05bbab27e2
Byron Ochara <byron.ochara@gmail.com>
2026-02-26 11:48:03 +0300

    created paymentResultSchema ts type

--- types/index.ts
+++ types/index.ts
@@ -6,6 +6,7 @@ import {
   shippingAddressSchema,
   insertOrderSchema,
   insertOrderItemSchema,
+  paymentResultSchema,
 } from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
@@ -35,3 +36,5 @@ export type Order = z.infer<typeof insertOrderSchema> & {
   orderitem: OrderItem[];
   user: { name: string; email: string };
 };
+
+export type PaymentResult = z.infer<typeof paymentResultSchema>;
