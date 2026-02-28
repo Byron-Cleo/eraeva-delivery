cc769b9166289b1db0a9b076b3538873dd0ab6d3
Byron Ochara <byron.ochara@gmail.com>
2026-02-28 07:59:49 +0300

    profile and orders pages setup

---
+++ app/user/main-nav.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import React from "react";
+import { title } from "process";
+
+const links = [
+  { title: "Profile", href: "/user/profile" },
+  { title: "Orders", href: "/user/orders" },
+];
+
+const MainNav = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) => {
+  return (
+    <nav className={cn("flex items-center space-x-4", className)} {...props}>
+        {links.map((link) => {
+            const pathname = usePathname();
+            const isActive = pathname === link.href;
+            return (
+                <Link
+                    key={link.href}
+                    href={link.href}
+                    className={cn(
+                      "text-sm font-medium transition-colors hover:text-primary",
+                      isActive ? "text-primary" : "text-muted-foreground"
+                    )}
+                >
+                  {link.title}
+                </Link>
+            );
+        })}
+    </nav>
+  );
+};
+
+export default MainNav;
---
+++ app/user/profile/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "User Profile",
+};
+
+const ProfilePage = () => {
+  return <>Profile Page</>;
+};
+
+export default ProfilePage;
