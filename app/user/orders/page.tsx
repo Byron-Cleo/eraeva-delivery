40e2c42886ee2c9b54a9194d6bba02f7b13f4bf3
Byron Ochara <byron.ochara@gmail.com>
2026-02-28 14:40:53 +0300

    pagination changes

--- app/user/orders/page.tsx
+++ app/user/orders/page.tsx
@@ -2,7 +2,15 @@ import { Metadata } from "next";
 import { getMyOrders } from "@/lib/actions/order.actions";
 import { formatCurrency, formatDateTime, formatId } from "@/lib/utils";
 import Link from "next/link";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Pagination from "@/components/shared/pagination/pagination";
 
 export const metadata: Metadata = {
   title: "My Orders",
@@ -20,34 +28,55 @@ const OrdersPage = async (props: {
       <h2 className="h2-bold">Orders</h2>
       <div className="autoflow-auto">
         <Table>
-            <TableHeader>
-
-                <TableRow>
-                    <TableHead>ID</TableHead>
-                    <TableHead>DATE</TableHead>
-                    <TableHead>TOTAL</TableHead>
-                    <TableHead>PAID</TableHead>
-                    <TableHead>DELIVERED</TableHead>
-                    <TableHead>ACTIONS</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {orders.data.map((order) => (
-                    <TableRow key={order.id}>
-                        <TableCell className="font-medium">{formatId(order.id)}</TableCell>
-                        <TableCell>{formatDateTime(order.createdAt).dateTime}</TableCell>
-                        <TableCell>{formatCurrency(order.totalPrice)}</TableCell>
-                        <TableCell>{order.isPaid && order.paidAt ? formatDateTime(order.paidAt).dateTime : "Not Paid"}</TableCell>
-                        <TableCell>{order.isDelivered && order.deliveredAt ? formatDateTime(order.deliveredAt).dateTime : "Not Delivered"}</TableCell>
-                        <TableCell>
-                            <Link href={`/order/${order.id}`} className="text-sm text-primary font-medium">
-                                Details
-                            </Link>
-                        </TableCell>
-                    </TableRow>
-                ))}
-            </TableBody>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>DATE</TableHead>
+              <TableHead>TOTAL</TableHead>
+              <TableHead>PAID</TableHead>
+              <TableHead>DELIVERED</TableHead>
+              <TableHead>ACTIONS</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {orders.data.map((order) => (
+              <TableRow key={order.id}>
+                <TableCell className="font-medium">
+                  {formatId(order.id)}
+                </TableCell>
+                <TableCell>
+                  {formatDateTime(order.createdAt).dateTime}
+                </TableCell>
+                <TableCell>{formatCurrency(order.totalPrice)}</TableCell>
+                <TableCell>
+                  {order.isPaid && order.paidAt
+                    ? formatDateTime(order.paidAt).dateTime
+                    : "Not Paid"}
+                </TableCell>
+                <TableCell>
+                  {order.isDelivered && order.deliveredAt
+                    ? formatDateTime(order.deliveredAt).dateTime
+                    : "Not Delivered"}
+                </TableCell>
+                <TableCell>
+                  <Link
+                    href={`/order/${order.id}`}
+                    className="text-sm text-primary font-medium"
+                  >
+                    Details
+                  </Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
         </Table>
+        {orders.totalPages > 1 && (
+          <Pagination
+            totalPages={orders?.totalPages}
+            page={Number(page) || 1}
+            urlParamName="page"
+          />
+        )}
       </div>
     </div>
   );
