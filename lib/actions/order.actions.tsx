ee5b585e7755ad339040e0a3bbe85781e52cfda2
Byron Ochara <byron.ochara@gmail.com>
2026-02-28 08:00:33 +0300

    orders action for getting cart

--- lib/actions/order.actions.tsx
+++ lib/actions/order.actions.tsx
@@ -10,6 +10,7 @@ import { prisma } from "@/db/prisma";
 import { CartItem, PaymentResult } from "@/types";
 import { paypal } from "../paypal";
 import { revalidatePath } from "next/cache";
+import { PAGE_SIZE } from "../constants";
 
 //create order and create the order items
 export async function createOrder() {
@@ -249,4 +250,32 @@ async function updateOrderToPaid({
   });
 
   if (!updatedOrder) throw new Error("Order not found.");
+}
+
+//get user's orders
+export async function getMyOrders({limit=PAGE_SIZE, page}: {limit?: number, page: number}) {
+  const session = await auth();
+  if (!session) throw new Error("User is not authorized");
+
+  const data = await prisma.order.findMany({
+    where: { userId: session?.user?.id! },
+    orderBy: { createdAt: "desc" },
+    include: { orderitem: true },
+    take: limit,
+    skip: (page - 1) * limit,
+  });
+
+  const totalOrders =await prisma.order.count({ where: { userId: session?.user?.id! } });
+
+  // const totalOrders = await prisma.order.count({ where: { userId: session.user.id } });
+  // const totalPages = Math.ceil(totalOrders / limit);
+
+
+  // const totalOrders = await prisma.order.count({ where: { userId } });
+  const totalPages = Math.ceil(totalOrders / PAGE_SIZE);
+
+  return {
+    data: convertToPlainObject(data),
+    totalPages,
+  };
 }
