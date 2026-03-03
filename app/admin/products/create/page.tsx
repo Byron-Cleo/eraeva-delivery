2f6f824a7ee2a98b3628f9ed102caa976d217af7
Byron Ochara <byron.ochara@gmail.com>
2026-03-03 22:31:52 +0300

    create product form page

---
+++ app/admin/products/create/page.tsx
@@ -0,0 +1,20 @@
+import ProductForm from "@/components/admin/product-form";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Admin Dashboard",
+  description: "Create Product Page",
+};
+
+const CreateProductPage = () => {
+  return (
+    <>
+      <h2 className="h2-bold">Create Product</h2>
+      <div className="my-8">
+        <ProductForm type="Create"/>
+      </div>
+    </>
+  );
+};
+
+export default CreateProductPage;
