f64b7d3cbf7807008238ee1d687eb29eb713bb07
Byron Ochara <byron.ochara@gmail.com>
2026-03-03 13:05:34 +0300

    update schema extended

--- lib/validators.ts
+++ lib/validators.ts
@@ -23,6 +23,9 @@ export const insertProductSchema = z.object({
   price: currency,
 });
 
+//schema for updating products
+export const updateProductSchema = insertProductSchema.extend({ id: z.string().min(1, "ID is required")})
+
 //Schema for signing users in
 export const signInFormSchema = z.object({
   email: z.string().email("Invalid email address."),
