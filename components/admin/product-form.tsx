145ef6e74329921ff9555676863bd5f947d5f5a6
Byron Ochara <byron.ochara@gmail.com>
2026-03-04 12:12:53 +0300

    implemented banner function

--- components/admin/product-form.tsx
+++ components/admin/product-form.tsx
@@ -18,6 +18,7 @@ import {UploadButton} from "@/lib/uploadthing"
 import { Card, CardContent } from "../ui/card";
 import Image from "next/image";
 import { Upload } from "lucide-react";
+import { Checkbox } from "../ui/checkbox";
 
 
 const ProductForm = ({
@@ -83,6 +84,8 @@ const ProductForm = ({
   }
 
   const images = form.watch("images")
+  const isFeatured = form.watch("isFeatured")
+  const banner = form.watch("banner")
 
   return (
     <Form {...form}>
@@ -220,8 +223,49 @@ const ProductForm = ({
           )}
           />
         </div>
-        <div className="flex flex-col md:flex-row gap-5">
+        {/* <div className="flex flex-col md:flex-row gap-5"> */}
+        <div className="upload-field">
           {/* isFeatured */}
+          Feature Product 
+          <Card>
+            <CardContent className="space-y-2 mt-2">
+              <FormField
+                control={form.control}
+                name="isFeatured"
+                render={({ field }) => (
+                  <FormItem className="space-x-2 flex items-center">
+                    <FormControl>
+                      <Checkbox
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                    <FormLabel>Is Featured</FormLabel>
+                  </FormItem>
+                )}
+              />
+              {isFeatured && banner && (
+                <Image
+                src={banner}
+                alt="banner image"
+                className="w-full object-cover object-center rounded-sm"
+                width={1920}
+                height={680}
+                />
+              )}
+              {isFeatured && !banner && (
+                <UploadButton className="upload-field" endpoint="imageUploader" onClientUploadComplete={(res: {url: string}[]) => {
+                  form.setValue("banner", res[0].url)
+                }}
+                onUploadError={(error: Error) => {
+                  toast({
+                    variant: "destructive",
+                    description: `Error! ${error.message}`
+                  })
+                }}/>
+              )}
+            </CardContent>
+          </Card>
         </div>
         <div>
           {/* description */}
