a316179ecbb7b006d395f976e1ac6acdd09aa2ee
Byron Ochara <byron.ochara@gmail.com>
2026-03-04 18:38:51 +0300

    update user action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import {
   shippingAddressSchema,
   signInFormSchema,
   signUpFormSchema,
+  updateUserSchema,
 } from "../validators";
 import { auth, signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect";
@@ -217,3 +218,22 @@ export async function deleteUser(id: string) {
     return { success: false, message: formatError(error) };
   }
 }
+
+ //update user
+  export async function updateUser(user: z.infer<typeof updateUserSchema>) {
+    try {
+      await prisma.user.update({
+        where: {id: user.id},
+        data: {
+          name: user.name,
+          role: user.role,
+        }
+      })
+
+      revalidatePath("/admin/users");
+
+      return {success: true, message: "User updated successfully"}
+    } catch (error) {
+      return {success: false, message: formatError(error)}
+    }
+  }
