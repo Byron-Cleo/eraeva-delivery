de4bc85079aefd526986dba6668239a19372492c
Byron Ochara <byron.ochara@gmail.com>
2026-03-05 17:09:03 +0300

    update product  stock validation

--- lib/validators.ts
+++ lib/validators.ts
@@ -17,7 +17,7 @@ export const insertProductSchema = z.object({
   brand: z.string().min(3, "Brand must be at least 3 characters"),
   description: z.string().min(3, "Description must be at least 3 characters"),
   // stock: z.coerce.number(),
-  stock: z.coerce.number<string>(),
+  stock: z.coerce.number<number>(),
   images: z.array(z.string()).min(1, "Product must have at least one image"),
   isFeatured: z.boolean(),
   banner: z.string().nullable(),
