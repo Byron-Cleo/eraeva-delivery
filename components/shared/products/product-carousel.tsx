5e4d3d90d005d291c3a720dc0d1e07e04b48b607
Byron Ochara <byron.ochara@gmail.com>
2026-03-06 12:33:18 +0300

    time chag and code update

--- components/shared/products/product-carousel.tsx
+++ components/shared/products/product-carousel.tsx
@@ -19,7 +19,7 @@ const ProductCarousel = ({ data }: { data: Product[] }) => {
       opts={{ loop: true }}
       plugins={[
         Autoplay({
-          delay: 10000,
+          delay: 3000,
           stopOnInteraction: true,
           stopOnMouseEnter: true,
         }),
@@ -39,7 +39,7 @@ const ProductCarousel = ({ data }: { data: Product[] }) => {
                   className="w-full h-auto"
                 />
                 <div className="absolute inset-0 flex items-end justify-center">
-                  <h2 className="bg-gray-900 bg-opacity-50 text-2xl font-bold px-2 text-white">
+                  <h2 className="px-2 text-2xl font-bold text-white bg-gray-900 bg-opacity-50">
                     {product.name}
                   </h2>
                 </div>
