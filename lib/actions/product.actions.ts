683eb9f54b1aa291e8aef8c4f1acc0c83baf61a4
Byron Ochara <byron.ochara@gmail.com>
2026-03-06 10:54:26 +0300

    featured products action

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -128,3 +128,15 @@ export async function getAllCategories() {
   });
   return data;
 }
+
+//get featurd products
+export async function getFeaturedProducts() {
+  const data = await prisma.product.findMany({
+    where: { isFeatured: true },
+    orderBy: { createdAt: "desc" },
+    take: 4
+  });
+
+  return data;
+  // return convertToPlainObject(data);
+}
