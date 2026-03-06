911979475f65958ec3c69cd1dd6c40c94112a20d
Byron Ochara <byron.ochara@gmail.com>
2026-03-06 14:49:40 +0300

    search components

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { APP_NAME } from "@/lib/constants";
 import Menu from "./menu";
 import CategoryDrawer from "./category-drawer";
+import Search from "./search";
 
 const Header = () => {
   return (
@@ -10,7 +11,7 @@ const Header = () => {
       <div className="wrapper flex-between">
         <div className="flex-start">
           <CategoryDrawer />
-          <Link href="/" className="flex-start ml-4">
+          <Link href="/" className="ml-4 flex-start">
             <Image
               src="/images/logo.svg"
               alt={`${APP_NAME} logo`}
@@ -18,10 +19,13 @@ const Header = () => {
               width={48}
               priority={true}
             />
-            <span className="hidden lg:block font-bold text-2xl ml-3">
+            <span className="hidden ml-3 text-2xl font-bold lg:block">
               {APP_NAME}
             </span>
           </Link>
+        </div>
+        <div className="hidden md:block">
+          <Search />
         </div>
         <Menu />
       </div>
