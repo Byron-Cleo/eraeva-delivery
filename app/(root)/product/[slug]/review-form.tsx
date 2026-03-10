72d382b0acb9ec2e2edf7e74c6eaa57c3e719158
Byron Ochara <byron.ochara@gmail.com>
2026-03-10 21:44:38 +0300

    form for submitting reviews

---
+++ app/(root)/product/[slug]/review-form.tsx
@@ -0,0 +1,173 @@
+"use client";
+
+import { useToast } from "@/hooks/use-toast";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { insertReviewSchema } from "@/lib/validators";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { reviewFormDefaultvalues } from "@/lib/constants";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+} from "@/components/ui/select";
+import { StarIcon } from "lucide-react";
+import { createUpdateReview } from "@/lib/actions/review.actions";
+
+const ReviewForm = ({
+  userId,
+  productId,
+  onReviewSubmitted,
+}: {
+  userId: string;
+  productId: string;
+  onReviewSubmitted: () => void;
+}) => {
+  const [open, setOpen] = useState(false);
+  const { toast } = useToast();
+
+  const form = useForm<z.infer<typeof insertReviewSchema>>({
+    resolver: zodResolver(insertReviewSchema),
+    defaultValues: reviewFormDefaultvalues,
+  });
+
+  //Open Form handler
+  const handleOpenForm = () => {
+    // this is same as seting the form values using the form attribute which distinguishes the
+    //different form values, BUT NOW, DOING IT MANUALLY IN THE CODE INSTAED OF USER INPUT.
+    form.setValue("productId", productId);
+    form.setValue("userId", userId);
+
+    //then open the form modal
+    setOpen(true);
+  };
+
+  //Submit form handler, i.e submitting the form data to the database for storage
+  //The values being passed by default is the form values input from the user
+  const onSubmit: SubmitHandler<z.infer<typeof insertReviewSchema>> = async (
+    values,
+  ) => {
+    console.log("VVVV===>", values)
+    //then now call the review action, to create or update the review in the database
+    const res = await createUpdateReview({ ...values, productId });
+
+    if (!res.success) {
+      return toast({
+        variant: "destructive",
+        description: res.message,
+      });
+    }
+    setOpen(false);
+
+    onReviewSubmitted();
+
+    toast({
+      description: res.message,
+    });
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <Button onClick={handleOpenForm} variant="default">
+        Write a Review{" "}
+      </Button>
+      <DialogContent className="sm:max-w-[425px]">
+        <Form {...form}>
+          <form method="POST" onSubmit={form.handleSubmit(onSubmit)}>
+            <DialogHeader>
+              <DialogTitle>Write a Review</DialogTitle>
+              <DialogDescription>
+                Share your thoughts with other customers
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Title:</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter title" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Description:</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter description" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="rating"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Rating:</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      value={field.value.toString()}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <Input {...field} />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {Array.from({ length: 5 }).map((_, index) => (
+                          <SelectItem
+                            key={index}
+                            value={(index + 1).toString()}
+                          >
+                            {index + 1} <StarIcon className="inline h-4 w-4" />
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <Button
+              type="submit"
+              size="lg"
+              className="w-full"
+              disabled={form.formState.isSubmitting}
+            >
+              {form.formState.isSubmitting ? "Submitting..." : "Submit"}
+            </Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ReviewForm;
