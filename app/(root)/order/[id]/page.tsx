a0831d2b07cc1da609ba470ed45c3a987aa3e675
Byron Ochara <byron.ochara@gmail.com>
2026-03-11 22:34:14 +0300

    initialised stripe integration

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -4,6 +4,7 @@ import { notFound } from "next/navigation";
 import OrderDetailsTable from "./order-details-table";
 import { ShippingAddress } from "@/types";
 import { auth } from "@/auth";
+import Stripe from "stripe";
 
 export const metadata: Metadata = {
   title: "Order Details",
@@ -15,7 +16,22 @@ const OrderDetailsPage = async (props: { params: Promise<{ id: string }> }) => {
   const order = await getOrderById(id);
   if (!order) notFound();
 
-  const session = await auth()
+  const session = await auth();
+
+  let client_secret = null;
+
+  //check if is not paid and paymentMethod is using Stripe
+  if (order.paymentMethod === "Stripe" && !order.isPaid) {
+    //Initialize stripe instance
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+    //Create payment intent
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(Number(order.totalPrice) * 100),
+      currency: "usd",
+      metadata: { orderId: order.id },
+    });
+    client_secret = paymentIntent.client_secret;
+  }
 
   return (
     <>
@@ -24,6 +40,7 @@ const OrderDetailsPage = async (props: { params: Promise<{ id: string }> }) => {
           ...order,
           shippingAddress: order.shippingAddress as ShippingAddress,
         }}
+        stripeClientSecret={client_secret}
         paypalClientId={process.env.PAYPAL_CLIENT_ID || "sb"}
         isAdmin={session?.user?.role === "admin" || false}
       />
