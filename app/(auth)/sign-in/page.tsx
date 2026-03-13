345353ec23af378b9bb94de834f5e8fb3e1b72d2
Byron Ochara <byron.ochara@gmail.com>
2026-03-14 00:20:57 +0300

    token changes effect

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -17,11 +17,12 @@ export const metadata: Metadata = {
   title: "Sign In",
 };
 
-const SignInPage = async (props: {searchParams: Promise<{callbackUrl:string}>}) => {
+const SignInPage = async (props: {
+  searchParams: Promise<{ callbackUrl: string }>;
+}) => {
   const session = await auth();
 
   const { callbackUrl } = await props.searchParams;
-  
   //is there is a succefully logged in user, redirect to the home page
   if (session) {
     return redirect(callbackUrl || "/");
