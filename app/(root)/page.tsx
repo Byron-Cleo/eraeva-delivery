25384ddb2635b631cec8511da137df27a2624000
Byron Ochara <byron.ochara@gmail.com>
2026-03-15 17:48:27 +0300

    implemented project coutdown

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,6 +6,7 @@ import {
 import ProductCarousel from "@/components/shared/products/product-carousel";
 import ViewAllProductsButton from "@/components/view-all-products-button";
 import IconBoxes from "@/components/icon-boxes";
+import DealCountDown from "@/components/deal-countdown";
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
@@ -18,7 +19,7 @@ const Homepage = async () => {
       )}
       <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
       <ViewAllProductsButton />
-
+      <DealCountDown />
       <IconBoxes />
     </>
   );
