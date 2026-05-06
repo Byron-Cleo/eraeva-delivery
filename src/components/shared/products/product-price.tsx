3cdb11e2f3fa50ba8b0fbfaf4ef75d91f8ff90f0
Byron Ochara <byron.ochara@gmail.com>
2026-05-06 10:19:37 +0300

    changed money symbol

--- src/components/shared/products/product-price.tsx
+++ src/components/shared/products/product-price.tsx
@@ -1,17 +1,25 @@
 import { cn } from "@/lib/utils";
 
-const ProductPrice = ({value, className}: {value: number; className?: string}) => {
-    //unsure product price to 2 decimal places
-    const stringValue = value.toFixed(2)
+const ProductPrice = ({
+  value,
+  className,
+}: {
+  value: number;
+  className?: string;
+}) => {
+  //unsure product price to 2 decimal places
+  const stringValue = value.toFixed(2);
 
-    //get the integer and float values
-    const [intValue, floatvalue] = stringValue.split('.')
-    
-    return ( <p className={cn("text-2xl", className)}>
-        <span className="text-xs align-super">$</span>
-        {intValue}
-        <span className="text-xs align-super">.{floatvalue}</span>
-    </p> );
-}
- 
+  //get the integer and float values
+  const [intValue, floatvalue] = stringValue.split(".");
+
+  return (
+    <p className={cn("text-2xl", className)}>
+      <span className="text-xs align-super">KSh </span>
+      {" "}{intValue}
+      <span className="text-xs align-super">.{floatvalue}</span>
+    </p>
+  );
+};
+
 export default ProductPrice;
