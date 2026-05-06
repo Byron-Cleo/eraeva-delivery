553792e500d9242e200b71e8c9f84056c1934276
Byron Ochara <byron.ochara@gmail.com>
2026-05-06 09:14:31 +0300

    applied guard clause for resend

--- src/email/index.tsx
+++ src/email/index.tsx
@@ -1,14 +1,15 @@
 //this is where we initailize the resend library for sending emails
-import { Resend } from "resend";
 import { SENDER_EMAIL, APP_NAME } from "@/lib/constants";
 import { Order } from "@/types";
 import PurchaseReciptEmail from "./purchase-receipt";
 require("dotenv").config();
 
-//initailize the resend object
-const resend = new Resend(process.env.RESEND_API_KEY as string);
-
 export const sendPurchaseReceipt = async ({ order }: { order: Order }) => {
+  if (!process.env.RESEND_API_KEY) return;
+
+  const { Resend } = await import("resend");
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
   await resend.emails.send({
     from: `${APP_NAME} <${SENDER_EMAIL}>`,
     to: order.user.email,
